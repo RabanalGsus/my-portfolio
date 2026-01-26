dd60eca02e861f203dd4e75354106fbcd5752aaa
RabanalGsus <jesusrabanal@hotmail.com>
2026-01-26 15:04:17 +0100

    updated tags file

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -2,8 +2,16 @@ import type { ReactNode } from "react";
 
 export default function Tag({ children }: { children: ReactNode }) {
   return (
-    <span className="rounded-full border border-zinc-800 bg-zinc-900/40 px-2.5 py-1 text-xs text-zinc-200">
+    <span className="
+      rounded-full
+      border border-[rgb(var(--accent))]
+      bg-[rgb(var(--accent))]/10
+      px-2.5 py-1
+      text-xs
+      text-[rgb(var(--text))]
+    ">
       {children}
     </span>
+
   );
 }
